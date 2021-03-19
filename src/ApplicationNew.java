1f0298326472777c8eebdbb0c524f8cfad6b469f
Tempestas Ludi <tempestasludi@gmail.com>
2021-03-19 16:23:18 +0100

    Clean up ApplicationNew

--- src/ApplicationNew.java
+++ src/ApplicationNew.java
@@ -1,37 +1,24 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 
-public class ApplicationNew extends Frame implements KeyListener{
-    private Tile[] tiles;
-    JLabel label;
-
+public class ApplicationNew extends Frame implements KeyListener {
+    private final Tile[] tiles;
     private final int size = 50;
     private Image stageImage = null;
-    int TILES_X = 3, TILES_Y = 3;
-    int original_x = 0, original_y = 0;
-    Random random = new Random();
-    boolean globalBool = false;
-    int globalStageCount = 0;
+    private final Random random = new Random();
+    private boolean globalBool = false;
+    private int globalStageCount = 0;
+    private final Tile[][] fixedTiling;
 
-    public ApplicationNew(){
+    public ApplicationNew() {
         super("Java AWT Examples");
         prepareGUI();
-
-        tiles = new Tile[]{
-                new Tile(size, Color.RED, Color.GREEN, Color.RED, Color.GREEN),
-                new Tile(size, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW),
-                new Tile(size, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW),
-                new Tile(size, Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN),
-                new Tile(size, Color.BLUE, Color.GREEN, Color.BLUE, Color.GREEN),
-                new Tile(size, Color.BLUE, Color.GREEN, Color.RED, Color.YELLOW),
-                new Tile(size, Color.BLUE, Color.YELLOW, Color.BLUE, Color.YELLOW),
-                new Tile(size, Color.BLUE, Color.YELLOW, Color.RED, Color.GREEN),
-        };
-
+        tiles = new Tile[]{new Tile(size, Color.RED, Color.GREEN, Color.RED, Color.GREEN), new Tile(size, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW), new Tile(size, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW), new Tile(size, Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN), new Tile(size, Color.BLUE, Color.GREEN, Color.BLUE, Color.GREEN), new Tile(size, Color.BLUE, Color.GREEN, Color.RED, Color.YELLOW), new Tile(size, Color.BLUE, Color.YELLOW, Color.BLUE, Color.YELLOW), new Tile(size, Color.BLUE, Color.YELLOW, Color.RED, Color.GREEN),};
         Stage stage = new RandomPointsStage();
         addKeyListener(this);
+
+        this.fixedTiling = createTiling(10, 7);
 
         new Thread(() -> {
             while (true) {
@@ -46,235 +33,124 @@ public class ApplicationNew extends Frame implements KeyListener{
         }).start();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ApplicationNew awtGraphicsDemo = new ApplicationNew();
         awtGraphicsDemo.setVisible(true);
     }
 
-
-    private void prepareGUI(){
-        setSize(800,800);
+    private void prepareGUI() {
+        setSize(800, 800);
         addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent){
+            public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
     }
 
     @Override
-    public void paint(Graphics g){
-        //KeyEventInfo keyInfo = new KeyEventInfo("KeyEventInfo");
-        ///int id = keyInfo.getKeyTyped().getID();
+    public void paint(Graphics g) {
         int stageOne = 1;
         int stageTwo = 2;
-        if(globalBool== true){
-            if(globalStageCount == stageOne){
-                    paintFirstStage(g);
-            }
-            else if(globalStageCount == stageTwo){
+        if (globalBool) {
+            if (globalStageCount == stageOne) {
+                paintFirstStage(g);
+            } else if (globalStageCount == stageTwo) {
                 paintWangTiling(g);
-
             }
-
         }
 
-
-
+        drawTiling((Graphics2D)g, fixedTiling, 100, 400);
     }
 
     public void paintFirstStage(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         for (int i = 0; i < tiles.length; i++) {
             Tile tile = tiles[i];
             tile.draw(g2, 0, 100 + (int) (i * size * 1.1));
         }
-
         if (this.stageImage != null) {
             g2.drawImage(this.stageImage, 200, 200, null);
         }
     }
 
     public void paintWangTiling(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
-        Tile[][] globalPervUpperTile = new Tile[TILES_X][TILES_Y];
-        for(int row = 0; row < TILES_X; row++) {
-            Tile pervTile = null;
-            for (int i = 0; i < TILES_Y; i++) {
+        Graphics2D g2 = (Graphics2D) g;
+        int TILES_X = 3;
+        int TILES_Y = 3;
+        Tile[][] tiling = createTiling(TILES_X, TILES_Y);
 
-                Tile tile = tiles[i];
+        drawTiling(g2, tiling, 0, 100);
 
-                if(isUpperLeftCorner(row,i)){
-                    tile = getFirstTile(row,i);
-                    pervTile = tile;
-                    tile.draw(g2, 0, 100 + (int) (i * size * 1.1));
-                }
-                if(isFirstRow(row) == true && !isUpperLeftCorner(row,i) == true){
-                    tile = getTileFirstRow(row,i,pervTile);
-                    pervTile = tile;
-                    tile.draw(g2, (int) (i * size * 1.1), 100);
-
-                }
-                if(isRestRow(row) == true && isFirstTilePerRow(i) == true){
-                    if(row >= 0) {
-                        tile = getFirstTilePerRow(row, i, globalPervUpperTile[row - 1][i]);
-                        pervTile = tile;
-                        tile.draw(g2, 0, 100 + (int) (row * size * 1.1));
-                    }
-                }
-
-                if(isRestRow(row) == true && !isFirstTilePerRow(i) == true){
-                    if(row >= 0) {
-                        tile = getTileRestRow(row, i, pervTile,globalPervUpperTile[row - 1][i]);
-                        pervTile = tile;
-                        tile.draw(g2, (int) (i * size * 1.1), 100 + (int) (row * size * 1.1));
-                    }
-
-                }
-
-                globalPervUpperTile[row][i] = tile;
-                //tile.draw(g2, 100, 100 + (int) (i * size * 1.1));
-
-            }
-        }
         if (this.stageImage != null) {
             g2.drawImage(this.stageImage, 200, 200, null);
         }
     }
 
-
-
-    /*Check if the tile is in first row or not
-    *
-    *
-    * */
-    public boolean isFirstRow(int i)
-    {
-        boolean determineValue = false;
-
-        if(i == original_x){
-
-            determineValue = true;
+    public Tile[][] createTiling(int w, int h) {
+        Tile[][] tiling = new Tile[h][w];
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                if (y == 0 && x == 0) {
+                    tiling[y][x] = getFirstTile();
+                    continue;
+                }
+                if (y == 0) {
+                    tiling[y][x] = getTileFirstRow(tiling[y][x - 1]);
+                    continue;
+                }
+                if (x == 0) {
+                    tiling[y][x] = getFirstTilePerRow(tiling[y - 1][x]);
+                    continue;
+                }
+                tiling[y][x] = getTileRestRow(tiling[y][x - 1], tiling[y - 1][x]);
+            }
         }
-
-
-        return determineValue;
+        return tiling;
     }
 
-    public boolean isUpperLeftCorner(int i, int j){
-
-        boolean determineValue = false;
-
-        if(i == original_x && j == original_y){
-
-            return determineValue = true;
-
+    public void drawTiling(Graphics2D g, Tile[][] tiling, int x0, int y0) {
+        final int border = 1;
+        for (int y = 0; y < tiling.length; y++) {
+            for (int x = 0; x < tiling[y].length; x++) {
+                tiling[y][x].draw(g, x0 + x * (size + border), y0 + y * (size + border));
+            }
         }
-
-        return determineValue;
     }
 
-
-
-    public boolean isFirstTilePerRow(int j){
-
-        boolean determineValue = false;
-
-        if(j == original_y){
-
-            return determineValue = true;
-
-        }
-
-        return determineValue;
-
+    public Tile getFirstTile() {
+        return tiles[random()];
     }
 
+    /*        Second step for stochastic tiling algorithm     */
 
-
-    public boolean isRestRow(int i){
-        boolean determineValue = false;
-
-        if(i != original_x && i != TILES_X){
-
-            determineValue = true;
-
-        }
-
-        return determineValue;
-    }
-
-    public Tile getFirstTile(int i, int j){
-
-        boolean determine = isUpperLeftCorner(i, j);
-        Tile tileValue = null;
-        if(determine == true){
-
-            //for(int k = 0; k< tiles.length; k++){
+    public Tile getTileFirstRow(Tile PervLeftTile) {
+        while (true) {
             Tile tile = tiles[random()];
-            tileValue = tile;
-            //}
-
-        }
-
-        return tileValue;
-    }
-
-/*
-    Second step for stochastic tiling algorithm
-
- */
-    public Tile getTileFirstRow(int i, int j, Tile PervLeftTile){
-        Tile tileValue = null;
-        boolean stop = false;
-
-        while (stop != true) {
-            Tile tile = tiles[random()];
-            if(PervLeftTile.east == tile.west){
-                stop = true;
+            if (PervLeftTile.east == tile.west) {
                 return tile;
             }
         }
-        //for(int k = 0; k<tiles.length; k++){
-
-
-        //}
-
-        return tileValue;
     }
 
-    public Tile getFirstTilePerRow(int i, int j, Tile PervUpperTile){
-        Tile tileValue = null;
-        boolean stop = false;
-        while (stop != true) {
+    public Tile getFirstTilePerRow(Tile PervUpperTile) {
+        while (true) {
             Tile tile = tiles[random()];
-            if(PervUpperTile.south == tile.north){
-                stop = true;
+            if (PervUpperTile.south == tile.north) {
                 return tile;
             }
         }
-
-        return tileValue;
     }
 
-    public Tile getTileRestRow(int i, int j, Tile PervLeftTile, Tile PervUpperTile){
-
-        Tile tileValue = null;
-        boolean stop = false;
-        while (stop != true) {
+    public Tile getTileRestRow(Tile PervLeftTile, Tile PervUpperTile) {
+        while (true) {
             Tile tile = tiles[random()];
-            if(PervLeftTile.east == tile.west && PervUpperTile.south == tile.north){
-                stop = true;
+            if (PervLeftTile.east == tile.west && PervUpperTile.south == tile.north) {
                 return tile;
             }
         }
-
-        return tileValue;
-
     }
 
-
-    public int random()
-    {
+    public int random() {
         return random.nextInt(tiles.length);
     }
 
@@ -287,57 +163,20 @@ public class ApplicationNew extends Frame implements KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_RIGHT) {
             globalBool = true;
             globalStageCount++;
         }
-
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             globalBool = true;
             globalStageCount--;
         }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            globalBool = true;
-            globalStageCount++;
-        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            globalBool = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_LEFT) {
             globalBool = true;
         }
     }
-
-
-
-
-
-/*
-    @Override
-    public void keyTyped(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            System.out.println("Space key typed");
-        }
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            System.out.println("Space key pressed");
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            System.out.println("Space key Released");
-        }
-    }
-
- */
 }
